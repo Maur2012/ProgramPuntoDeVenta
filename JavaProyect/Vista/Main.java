ef2edb36b898a7d31920a02d027886e989f6e99c
Mauricio Emiliano Ramirez Ceciliano <143680683+Maur2012@users.noreply.github.com>
2024-11-08 01:15:31 +0000

    Creacion Main

---
+++ JavaProyect/Vista/Main.java
@@ -0,0 +1,11 @@
+package Vista;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
